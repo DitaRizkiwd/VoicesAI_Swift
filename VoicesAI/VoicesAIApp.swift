6ad5dc1f66865f96b037e4f53e9bfc3a170e4b35
DitaRizkiwd <rdita201@gmail.com>
2024-04-24 13:18:10 +0700

    Initial Commit

---
+++ VoicesAI/VoicesAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoicesAIApp.swift
+//  VoicesAI
+//
+//  Created by MACBOOK PRO on 24/04/24.
+//
+
+import SwiftUI
+
+@main
+struct VoicesAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
